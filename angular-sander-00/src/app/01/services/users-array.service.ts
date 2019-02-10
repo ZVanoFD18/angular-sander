1cbce609f35f153274c5ccf1425f5c57a07fd2d7
Zaglyada Ivan <ZVanoFD18@gmail.com>
2019-02-10 17:41:47 +0200

    '02\forms\user-add-form-group'/RAW: валидация взаимосвязанных полей + вложенная FormGroup

--- angular-sander-00/src/app/01/services/users-array.service.ts
+++ angular-sander-00/src/app/01/services/users-array.service.ts
@@ -25,7 +25,7 @@ export class UsersArrayService {
         return this.users;
     }
 
-    add(name, login, pass) {
+    add(name: string, login: string, pass: string) {
         const newUser = new User();
         newUser.name = name;
         newUser.login = login;
@@ -35,16 +35,16 @@ export class UsersArrayService {
 }
 
 export class User {
-    public name;
-    public login;
-    public passHash;
+    public name: string;
+    public login: string;
+    public passHash: string;
 
     /**
      * Возвращает простейший хеш строки
      * @param {string} str
      * @returns {string}
      */
-    static getHash(str = '') {
+    static getHash(str: string = ''): string {
         let result = 0;
         for (let i = 0, len = str.length; i < len; i++) {
             result += str[i].charCodeAt(0);
--- angular-sander-00/src/app/02/forms/user-add-form-group/user-add-form-group.component.ts
+++ angular-sander-00/src/app/02/forms/user-add-form-group/user-add-form-group.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
+import {FormControl, FormGroup, NgForm, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 import {UsersArrayService} from "../../../01/services/users-array.service";
 
 @Component({
@@ -14,8 +14,16 @@ export class UserAddFormGroupComponent implements OnInit {
         this.formAdd = new FormGroup({
             userName: new FormControl('', Validators.required),
             userLogin: new FormControl('', Validators.required),
-            userPass: new FormControl('', Validators.required),
-            userPassConfirm: new FormControl('', Validators.required)
+            pass: new FormGroup({
+                userPass: new FormControl('', Validators.required),
+                userPassConfirm: new FormControl('', [
+                    Validators.required,
+                    this.passConfirmValidator.bind(this)
+                ])
+            }, [
+                globalPassValidator,
+                this.passValidator
+            ])
         });
     }
 
@@ -27,7 +35,79 @@ export class UserAddFormGroupComponent implements OnInit {
         this.us.add(
             form.controls.userName.value,
             form.controls.userLogin.value,
-            form.controls.userPass.value
+            form.get('pass').get('userPass').value
         );
     }
+
+    fillTestData() {
+        console.log('fillTestData/formAdd', this.formAdd);
+        this.formAdd.patchValue({
+            userName: 'Вася',
+            userLogin: 'Vasya',
+            pass: {
+                userPass: 'Vasya',
+                userPassConfirm: 'Vasya'
+            }
+        });
+    }
+
+    isValid = false;
+
+    passValidator(g: FormGroup) {
+        if (g.get('userPass').value === g.get('userPassConfirm').value) {
+            return null;
+        }
+        return {
+            mismatch: true
+        }
+    }
+
+    passConfirmValidator(passConfirm: FormControl) {
+        if (!this.formAdd) {
+            return null;
+        }
+        if (this.formAdd.get('pass').get('userPass').value === this.formAdd.get('pass').get('userPassConfirm').value) {
+            return null;
+        }
+        return {
+            passConfirmNotSame: true
+        }
+    }
+
+    /**
+     *
+     * @returns {boolean}
+     */
+    isPassConfirmValid() {
+        //console.log('isPassConfirmValid/formAdd', this.formAdd);
+        // @TODO: this.formGroup.get('name of you control').value
+        // console.log('userName', this.formAdd.get('userName'));
+        // console.log('userPass', this.formAdd.get('userPass'));
+        // if (this.formAdd.controls.pass.controls.userPass.value !== this.formAdd.controls.pass.controls.userPassConfirm.value) {
+        //     return false;
+        // }
+        return true;
+    }
+
+    isFormValid() {
+        if (!this.formAdd.valid) {
+            return false;
+        }
+        // if (!this.isPassConfirmValid()) {
+        //     return false;
+        // }
+        return true;
+    }
+    get myField() {
+        return this.formAdd.controls;
+    }
 }
+
+const globalPassValidator: ValidatorFn = (g: FormGroup): ValidationErrors | null => {
+    if (g.get('userPass').value === g.get('userPassConfirm').value) {
+        return null;
+    }
+    return {
+        mismatch: true
+    }
+};
